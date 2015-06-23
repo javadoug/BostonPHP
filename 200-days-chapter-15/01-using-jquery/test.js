79709ad337eb570376e3d1e427bfd3032db93a47
OnVelocity <onvelocity@OnVelocitys-MacBook-Pro.local>
2015-06-22 23:13:53 -0400

    chapter 15 exercises

---
+++ 200-days-chapter-15/01-using-jquery/test.js
@@ -0,0 +1,13 @@
+// Script 15.2 - test.js
+// This script is included by test.html.
+// This script just creates an alert to test jQuery.
+
+// Do something when the document is ready:
+$(function() {
+	
+	// aside: $(document).ready(function(){});
+
+	// Alert!
+	alert('Ready!');
+	
+});
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BostonPHP
+----
+200 Days of Coding presentation on Chapters 15 and 16 of http://www.larryullman.com/books/php-and-mysql-for-dynamic-web-sites-visual-quickpro-guide-4th-edition/
+
